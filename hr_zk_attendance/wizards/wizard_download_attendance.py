4c2e2709c039415a33d60c7a3c0b43342a397d89
Ubay Abdelgadir <obayit@gmail.com>
2021-01-07 18:50:08 +0200

    [IMP] hr_zk_attendance: make start and end of the month configurable
    defautls to start on 21st, and ends at 20th

--- hr_zk_attendance/wizards/wizard_download_attendance.py
+++ hr_zk_attendance/wizards/wizard_download_attendance.py
@@ -17,6 +17,21 @@ class DownloadAttendanceWizard(models.TransientModel):
 
     date_from = fields.Date(required=True, default=_get_default_date_from)
     date_to = fields.Date(required=True, default=_get_default_date_to)
+    duration_type = fields.Selection([
+        ('this_month', 'This Month'),
+        ('manual', 'Manual'),
+    ], default='this_month')
+
+    @api.onchange('duration_type')
+    def onchange_duration_type(self):
+        if not self.duration_type:
+            return
+        if self.duration_type == 'this_month':
+            firstDay = datetime.date.today().replace(day=1)
+            lastMonth = firstDay - datetime.timedelta(days=1)
+            ir_config = self.env['ir.config_parameter'].sudo()
+            self.date_from = lastMonth.replace(day=int(ir_config.get_param('hr_zk_attendance.month_start', 21)))
+            self.date_to = firstDay.replace(day=int(ir_config.get_param('hr_zk_attendance.month_end', 20)))
 
     def download_data(self):
         self.env['zk.machine'].browse(self.env.context['active_id']).download_attendance(
