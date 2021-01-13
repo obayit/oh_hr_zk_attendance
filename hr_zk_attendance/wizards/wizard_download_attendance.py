f199b9adcd77c90a58e5fa386e86f3c6ae94f0c4
Ubay Abdelgadir <obayit@gmail.com>
2021-01-13 11:56:33 +0200

    [IMP] hr_zk_attendance: handle months ranges properly in wizard

--- hr_zk_attendance/wizards/wizard_download_attendance.py
+++ hr_zk_attendance/wizards/wizard_download_attendance.py
@@ -28,10 +28,26 @@ class DownloadAttendanceWizard(models.TransientModel):
             return
         if self.duration_type == 'this_month':
             firstDay = datetime.date.today().replace(day=1)
-            lastMonth = firstDay - datetime.timedelta(days=1)
+            lastMonthLastDay = firstDay - datetime.timedelta(days=1)
             ir_config = self.env['ir.config_parameter'].sudo()
-            self.date_from = lastMonth.replace(day=int(ir_config.get_param('hr_zk_attendance.month_start', 21)))
-            self.date_to = firstDay.replace(day=int(ir_config.get_param('hr_zk_attendance.month_end', 20)))
+            
+            # make sure start day is in range of it's month
+            day_start = int(ir_config.get_param('hr_zk_attendance.month_start', 21))
+            day_start_ranage = calendar.monthrange(lastMonthLastDay.year, lastMonthLastDay.month)
+            if not (day_start_ranage[0] <= day_start <= day_start_ranage[1]):
+                day_start = day_start_ranage[0]
+            self.date_from = lastMonthLastDay.replace(day=day_start)
+
+            # make sure end day is in range of it's month
+            day_end = int(ir_config.get_param('hr_zk_attendance.month_end', 20))
+            day_end_ranage = calendar.monthrange(firstDay.year, firstDay.month)
+            if not (day_end_ranage[0] <= day_end <= day_end_ranage[1]):
+                day_end = day_end_ranage[1]
+            self.date_to = firstDay.replace(day=day_end)
+
+            # make sure the duration is not more than 31 days
+            if (self.date_to - self.date_from).days > 31:
+                self.date_to = lastMonthLastDay
 
     def download_data(self):
         self.env['zk.machine'].browse(self.env.context['active_id']).download_attendance(
